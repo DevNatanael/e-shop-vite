0a4c130788468becd90d0c8c6143bb202e117be7
Natanael <natanaelotavianoguedes@gmail.com>
2022-11-28 16:44:02 -0300

    cards feitos

--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
+export const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+  cursor: pointer;
+
+`;
+
+
 export const ContainerProduct = styled.div`
   flex: 1;
   margin: 5px;
@@ -9,6 +27,10 @@ export const ContainerProduct = styled.div`
   align-items: center;
   justify-content: center;
   position: relative;
+
+  &:hover ${Info}{
+    opacity:1;
+  }
 `;
 
 export const Container = styled.div`
@@ -19,8 +41,8 @@ export const Container = styled.div`
 `;
 
 export const Circle = styled.div`
-  width: 200px;
-  height: 200px;
+  width: 100px;
+  height: 100px;
   border-radius: 50%;
   background-color: white;
   position: absolute;
@@ -31,18 +53,19 @@ export const Image = styled.img`
   z-index: 2;
 `;
 
-export const Info = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: rgba(0,0,0,0.2);
-  z-index: 3;
-  display: flex;
-  align-items: center;
-  justify-content:center;
-`;
+// export const Info = styled.div`
+//   opacity: 0;
+//   width: 100%;
+//   height: 100%;
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   background-color: rgba(0, 0, 0, 0.2);
+//   z-index: 3;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+// `;
 
 export const Icon = styled.div`
   display: flex;
