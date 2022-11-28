553354370cc61ae15acd5239f6b55b442f944fc2
Natanael <natanaelotavianoguedes@gmail.com>
2022-11-28 15:38:51 -0300

    cards quase feitos

--- src/components/Categories/styles.js
+++ src/components/Categories/styles.js
@@ -45,8 +45,11 @@ export const Button = styled.button`
   background-color: #f3a847;
   color: black;
   font-weight: bold;
-  transition-duration: 0.4s;
-  :hover {
-  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.24);
-}
+  transition-duration: 0.5s;
+
+  &:hover {
+    background-color: rgba(0,0,0,0.2);
+    transform: scale(1.1);
+    color:white;
+  }
 `;
--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -1,32 +1,62 @@
 import styled from "styled-components";
 
-
 export const ContainerProduct = styled.div`
-    flex:1;
-    margin: 5px;
-    min-height:280px;
-    height: 350px;
-`
+  flex: 1;
+  margin: 5px;
+  min-height: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
 
 export const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-wrap:wrap;
-    justify-content:space-between;
-`
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
 
 export const Circle = styled.div`
-
-`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+`;
 
 export const Image = styled.img`
-    height: 65%;
-`
+  height: 65%;
+  z-index: 2;
+`;
 
 export const Info = styled.div`
-
-`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0,0,0,0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content:center;
+`;
 
 export const Icon = styled.div`
+  display: flex;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  transition: all 0.5s;
 
-`
+  &:hover {
+    background-color: #f3a847;
+    transform: scale(1.1);
+  }
+`;
